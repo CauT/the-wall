c4b5f3bba6c9b4a9ede918a54dc1a1db4e7d5161
CauT <donglingyongadls@gmail.com>
2016-03-23 13:38:29 +0800

    use ejs instead of jade

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ oracledb.getConnection(
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
