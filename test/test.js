acfd5b25a9bd1c947412eeaaef26ff7b5e734adc
CauT <donglingyongadls@gmail.com>
2016-04-13 20:24:08 +0800

    little modification on test.js

--- test/test.js
+++ test/test.js
@@ -16,7 +16,6 @@ function getJsonAndTest(done, path, equal) {
   var req = http.request(options, function(res) {
     res.setEncoding('utf8');
     res.on('data', function(chunk) {
-      console.log('BODY: ' + chunk);
       assert.equal(chunk, equal);
       done();
     })
@@ -32,14 +31,14 @@ describe('/v1', function() {
     describe('/info', function() {
       // to test path:/v1/device/info/type_list
       describe('/type_list', function() {
-        it('should return json as expected:', function(done) {
+        it('should return json as expected', function(done) {
           getJsonAndTest(done, '/v1/device/info/type_list',
             supposedJson.device.type_list);
         });
       });
       // to test path:/v1/device/info/station_list
       describe('/station_list', function() {
-        it('should return json as expected:', function(done) {
+        it('should return json as expected', function(done) {
           getJsonAndTest(done, '/v1/device/info/station_list',
             supposedJson.device.station_list);
         });
@@ -49,15 +48,15 @@ describe('/v1', function() {
   // to test path:/v1/data/agri_env/current?deviceType={}&stationName={}
   describe('/data', function() {
     describe('/agri_env', function() {
-      it('query with specified deviceType:', function(done) {
+      it('query with specified deviceType', function(done) {
         getJsonAndTest(done, '/v1/data/agri_env/current?deviceType=Light',
           supposedJson.agri_env.current_with_deviceName_Light);
       });
-      it('query with specified stationName:', function(done) {
+      it('query with specified stationName', function(done) {
         getJsonAndTest(done, '/v1/data/agri_env/current?stationName=A1',
           supposedJson.agri_env.current_with_stationName_A1);
       });
-      it('query with specified deviceType and stationName:', function(done) {
+      it('query with specified deviceType and stationName', function(done) {
         getJsonAndTest(done,
           '/v1/data/agri_env/current?deviceType=Light&stationName=A1',
           supposedJson.agri_env.current_with_deviceName_Light_and_stationName_A1
@@ -68,7 +67,7 @@ describe('/v1', function() {
   // to test path:/v1/utils/generate_graph?start_time=1443745800&end_time=1443760000&device_id=172&width=900&height=600
   describe('/utils', function() {
     describe('generate_graph', function() {
-      it('query with specified start_time and end_time and **single** device_id:',
+      it('query with specified start_time and end_time and **single** device_id',
       function(done) {
         var filePath = path.join(__dirname, 'supposedHtmlForGenerateGraph.html');
         fs.readFileAsync(filePath, 'utf-8')
@@ -83,7 +82,7 @@ describe('/v1', function() {
           console.log(err);
         });
       });
-      it('query with specified start_time and end_time and **multi** device_id:',
+      it('query with specified start_time and end_time and **multi** device_id',
       function(done) {
         var filePath = path.join(__dirname, 'supposedHtmlForGenerateMultiCurveGraph.html');
         fs.readFileAsync(filePath, 'utf-8')
